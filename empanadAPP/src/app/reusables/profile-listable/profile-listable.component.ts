4b425318e7ccbf7c20871618242a6391665fb746
Santiago <santiagosalinasoto@gmail.com>
2023-11-05 23:50:32 -0300

    Redo Selection, cant bind ngModel to mat-checkbox for unknown reason

--- empanadAPP/src/app/reusables/profile-listable/profile-listable.component.ts
+++ empanadAPP/src/app/reusables/profile-listable/profile-listable.component.ts
@@ -7,23 +7,27 @@ import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { Profile } from 'src/app/models/profile';
-//import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-profile-listable',
   templateUrl: './profile-listable.component.html',
   styleUrls: ['./profile-listable.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, MatCardModule, MatFormFieldModule,MatIconModule, MatInputModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatCardModule, MatFormFieldModule,MatIconModule, MatInputModule,MatCheckboxModule],
 })
 export class ProfileListableComponent {
   @Input() profileData: Profile = new Object() as Profile;
   @Input() pageInject: Profile[] = [];
 
-  active: boolean = false;
+  activo: boolean;
 
+  constructor() {
+    this.activo = false;
+  }
   getTotalQuantity(empanadaList: any[]): number {
     return empanadaList.reduce((total, empanada) => total + empanada.quantity, 0);
   }
@@ -35,4 +39,9 @@ export class ProfileListableComponent {
     }, 0);
   }
   
+  changeState(){
+    alert(this.activo)
+    this.activo = !this.activo;
+    alert(this.activo)
+  }
 }
